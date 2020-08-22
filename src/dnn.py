42493b3863014d93008286a9204abf97639c8d76
Liam-Desktop <liamgooch@live.com.au>
2020-08-22 17:49:22 +1000

    Update dnn.py

--- src/dnn.py
+++ src/dnn.py
@@ -32,9 +32,7 @@ class dnn:
 
 X = np.array([[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 1]])
 
-weights = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6],
-                    [0.7, 0.8, 0.9], [0.7, 0.8, 0.9]])
-bias = 0
-
 if __name__ == "__main__":
     l = layer(4, 4)
+    l.foward(X)
+    print(l.output)
